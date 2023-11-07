91fb2ce041804a366053f8983ca28d6f8e22dcb0
isieun0908 <supiya0908@naver.com>
2023-11-07 21:15:06 +0900

    feat: API

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import GPT2Model
+import Question
+
 app = FastAPI()
 
 class Text(BaseModel):
@@ -8,5 +11,7 @@ class Text(BaseModel):
 
 @app.get("/text")
 async def classify_text(text: Text):
+    label = GPT2Model.find_classification(text.text)
+    question = Question.find_question(label)
 
-    return {"message": text}
+    return {"label": label, "question": question}
